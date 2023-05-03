7d43972cb67a2d3df7e4740d089aebeb7b6c98a7
dannycoss42 <132470219+dannycoss42@users.noreply.github.com>
2023-05-03 11:17:39 -0400

    Created a new data base

---
+++ integration.py
@@ -0,0 +1,16 @@
+import duckdb
+
+#connect to a database file
+con = duckdb.connect('mydatabase.db')
+
+con.exectute("CREATE TABLE mytable (id INTEGER PRIMARY KEY, name VARCHAR(50), age INTEGER)")
+
+# Inserting data into the table
+con.execute("INSERT INTO mytable (id, name, age) VALUES (1, 'Lea', 21)")
+con.execute("INSERT INTO mytable (id, name, age) VALUES (2, 'Kevin', 22)")
+con.execute("INSERT INTO mytable (id, name, age) VALUES (3, 'Hung', 31)")
+
+# Printing our data
+result = con.execute("SELECT * FROM mytable")
+for row in result.fetchall():
+    print(row)
